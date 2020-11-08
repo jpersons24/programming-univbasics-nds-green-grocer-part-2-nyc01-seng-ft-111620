cf214f886f30ce36edaf4e042a88c935f0378d63
Jakob Persons <jakobep24@gmail.com>
2020-11-08 16:07:22 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -1,16 +1,63 @@
 require_relative './part_1_solution.rb'
 
+# takes a hash and applies coupons
+# return: a new array
+# coupon structure: []{:item => "AVACADO", :num => 2, :cost => 5.00}]
+
 def apply_coupons(cart, coupons)
   # Consult README for inputs and outputs
   #
   # REMEMBER: This method **should** update cart
+  counter = 0
+  while counter < coupons.length
+
+    # check for specific item within the cart
+    cart_item = find_item_by_name_in_collection(coupons[counter][:item], cart)
+    coupon_item_name = "#{coupons[counter][:item]} W/COUPON"
+    cart_item_with_coupon = find_item_by_name_in_collection(coupon_item_name, cart)
+
+    if cart_item && cart_item[:count] >= coupons[counter][:num]
+      if cart_item_with_coupon
+        cart_item_with_coupon[:count] += coupons[counter][:num]
+        cart_item[:count] -= coupons[counter][:num]
+      else
+        cart_item_with_coupon = {
+          :item => coupon_item_name,
+          :price => coupons[counter][:cost] / coupons[counter][:num],
+          :count => coupons[counter][:num],
+          :clearance => cart_item[:clearance]
+        }
+        cart << cart_item_with_coupon
+        cart_item[:count] -= coupons[counter][:num]
+      end
+    end
+    counter += 1
+  end
+  cart
 end
+
+# takes a hash and applies discounts for clearance items
+# return:
 
 def apply_clearance(cart)
   # Consult README for inputs and outputs
   #
   # REMEMBER: This method **should** update cart
+  counter = 0
+  while counter < cart.length
+      if cart[counter][:clearance]
+        cart[counter][:price] = (cart[counter][:price] - (cart[counter][:price] * 0.20)).round(2)
+      end
+    counter += 1
+  end
+  cart
 end
+
+# takes unconsolidated cart, consolidates it, applys coupons, and applys clearance
+# runs consolidated_cart, apply_coupons and apply_clearance
+# totals sum of all items and applys 'total price' discount if available
+# return: final cost
+
 
 def checkout(cart, coupons)
   # Consult README for inputs and outputs
@@ -22,4 +69,16 @@ def checkout(cart, coupons)
   #
   # BEFORE it begins the work of calculating the total (or else you might have
   # some irritated customers
+
+  consolidated_cart = consolidate_cart(cart)
+
+  consolidated_cart_with_coupons = apply_coupons(consolidated_cart, coupons)
+
+  consolidated_and_discounts_applied = apply_clearance(consolidated_cart_with_coupons)
+
+  consolidated_and_discounts_applied.each do |grocery_item|
+    grand_total = grocery_item[:price] * grocery_item[:count]
+  end
+
+
 end
